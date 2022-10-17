862a37eff93c2b9fb0e02a045deb03951e6434e7
Fadlan Ariel Fathurrahman <fadlan.ariel@ui.ac.id>
2022-10-17 13:16:33 +0700

    Initiate app

---
+++ my_account/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from my_account.views import *
+
+app_name = 'my_account'
+
+urlpatterns = [
+    path('', show_my_account, name='my_account'),
+    
+]
---
+++ my_account/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+#@login_required(login_url='landing_page:login')
+def show_my_account(request):
+    return render(request, 'my_account.html')
+
+# def show_achievments(request):
+
+# def show_event_history(request):
