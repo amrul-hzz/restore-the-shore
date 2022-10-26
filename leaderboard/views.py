9c1c934373d75a26dd47cc5fc1a83b00a3133935
itsjamessw <james.smith@ui.ac.id>
2022-10-26 15:46:24 +0700

    fixed views and template

--- leaderboard/views.py
+++ leaderboard/views.py
@@ -8,7 +8,7 @@ from django.core.paginator import Paginator
 
 # Create your views here.
 def show_leaderboard(request):
-    data_leaderboard = UserAccount.objects.all().order_by('-user_point', '-user.date_joined') # Tambahkan berdasarkan jumlah join_event dari UserAccount
+    data_leaderboard = UserAccount.objects.all().order_by('-user_point', '-user__date_joined') # Tambahkan berdasarkan jumlah join_event dari UserAccount
     paginator = Paginator(data_leaderboard, 25) # Show 25 account per page.
 
     page_number = request.GET.get('page')
