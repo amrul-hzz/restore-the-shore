2d0954a658a777732fb8b2ff33d4b3949e335496
yeira.putri <yeira.putri@ui.ac.id>
2022-10-27 23:53:02 +0700

    Added timeline.html

--- project_django/urls.py
+++ project_django/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('example_app.urls')),
     path('create-event/', include('create_event.urls')),
+    path('timeline/', include('timeline.urls'))
 ]
--- timeline/views.py
+++ timeline/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from create_event.models import Event
 # Create your views here.
+
+def show_data(request):
+    
+    data_event = Event().objects.all()
+    response = {
+        'datalist':  data_event,
+ 
+    }  
+    
+    return render(request, "timeline.html", response)
+
+def delete_card(request, pk):
+    UMKM.objects.get(id=pk).delete()
+    return redirect('timeline:show_data')
