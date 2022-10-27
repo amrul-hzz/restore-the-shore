f2b63c0ee305dfe489d4ba9b9dd0e39d3b062811
maysha.haliza <maysha.haliza@ui.ac.id>
2022-10-28 06:47:18 +0700

    rev

--- landing_page/views.py
+++ landing_page/views.py
@@ -9,12 +9,11 @@ from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 
-@login_required(login_url='/welcome/login/')
 def welcome(request):
     context = {
-        'last_login': request.COOKIES['last_login'],
+        'last_login': request.COOKIES['last_login']
     }
-    return render(request, "welcome.html")
+    return render(request, "welcome.html", context)
 
 def register(request):
     form = UserCreationForm()
@@ -29,14 +28,14 @@ def register(request):
     context = {'form':form}
     return render(request, 'register.html', context)
 
-def login(request):
+def login_user(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
         user = authenticate(request, username=username, password=password)
         if user is not None:
-            login(request, user) # melakukan login terlebih dahulu
-            response = HttpResponseRedirect(reverse("wishlist:show_wishlist")) # membuat response
+            login(request, user)
+            response = HttpResponseRedirect(reverse("landing_page:welcome"))
             response.set_cookie('last_login', str(datetime.datetime.now())) # membuat cookie last_login dan menambahkannya ke dalam response
             return response
         else:
@@ -44,8 +43,8 @@ def login(request):
     context = {}
     return render(request, 'login.html', context)
 
-def logout(request):
+def logout_user(request):
     logout(request)
-    response = HttpResponseRedirect(reverse('landing_page:login'))
+    response = HttpResponseRedirect(reverse('landing_page:login_user'))
     response.delete_cookie('last_login')
     return response
