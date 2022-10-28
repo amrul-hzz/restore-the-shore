307c1152ac34026b0019d765f2a3846ad2d9f7d8
James Smith Wigglesworth <jamesmithwigglesworth@gmail.com>
2022-10-28 14:33:33 +0700

    repaired templates and added external css

--- leaderboard/urls.py
+++ leaderboard/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from leaderboard.views import show_leaderboard, show_json
+from django.conf import settings 
+from django.conf.urls.static import static 
+
 
 app_name = 'leaderboard'
 
--- leaderboard/views.py
+++ leaderboard/views.py
@@ -3,13 +3,15 @@ from my_account.models import UserAccount
 from django.http import HttpResponse
 from django.core import serializers
 from django.core.paginator import Paginator
-
-# TODO: import app dari fadlan
-
+from django.contrib.auth.models import User
+from leaderboard.models import LeaderBoard
+from leaderboard.forms import AccountForm
+from django.views.decorators.csrf import csrf_exempt
+    
 # Create your views here.
 def show_leaderboard(request):
-    data_leaderboard = UserAccount.objects.all().order_by('-user_point', '-user__date_joined') # Tambahkan berdasarkan jumlah join_event dari UserAccount
-    paginator = Paginator(data_leaderboard, 25) # Show 25 account per page.
+    data_leaderboard = User.objects.all() # Tambahkan berdasarkan jumlah join_event dari UserAccount
+    paginator = Paginator(data_leaderboard, 1) # Show 5 account per page.
 
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
@@ -20,7 +22,17 @@ def show_leaderboard(request):
     }
     return render(request, "leaderboard.html", context)
 
+@csrf_exempt
+def search(request):
+    form = AccountForm()
+    if request.method == "POST":
+        form = AccountForm(request.POST)
+        if form.is_valid():
+            form.save()
+    context = {'form':form}
+    return render(request, 'leaderboard.html', 'context')
+
 def show_json(request):
     if request.method == "GET":
-        data_leaderboard = UserAccount.objects.all().order_by('-user_point', '-user.date_joined') # Tambahkan berdasarkan jumlah join_event dari UserAccount
+        data_leaderboard = UserAccount.objects.all().order_by('-user_point', '-user__date_joined') # Tambahkan berdasarkan jumlah join_event dari UserAccount
         return HttpResponse(serializers.serialize("json", data_leaderboard), content_type="application/json")
