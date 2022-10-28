a55c335c228472efafe0cbd0e8b850600e018377
maysha.haliza <maysha.haliza@ui.ac.id>
2022-10-28 14:12:57 +0700

    add base

--- landing_page/models.py
+++ landing_page/models.py
@@ -5,11 +5,11 @@ from django.contrib.auth.models import User
 #from create_event.models import Event
 
 # Create your models here.
-class UserAccount(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    user_point = models.IntegerField(default = 0)
+#class UserAccount(models.Model):
+  #  user = models.ForeignKey(User, on_delete=models.CASCADE)
+   # user_point = models.IntegerField(default = 0)
     # import models dari Event
-    events_joined = models.ManyToManyField(Event)
+    #events_joined = models.ManyToManyField(Event)
 
     # post di forum yang author nya user ini bisa langsung cek dari database forum aja
 
