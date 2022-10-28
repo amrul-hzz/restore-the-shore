f1a84e602239c942bad59b7d6e2429ba00e15128
Fadlan Ariel <fadlan.ariel@ui.ac.id>
2022-10-28 14:16:06 +0700

    delete model UserAccount

--- my_account/views.py
+++ my_account/views.py
@@ -1,21 +1,18 @@
-import imp
-from multiprocessing import context
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from my_account.models import UserAccount
 from django.http import HttpResponse
 from django.core import serializers
-#from landing_page.models import UserAccount
+from landing_page.models import UserAccount
 
 # Create your views here.
 @login_required(login_url='landing_page:login_user')
 def show_account(request):  
     # setiap register buat juga object UserAccount caranya UserAccount.objects.create(user = ..., user_point = 0)
     # Tiap join event tambahin events_joined caranya UserAccount.objects.get(user = request.user).events_joined.add(Event) kayaknya
-    #data_user = UserAccount.objects.get(user = request.user)
+    data_user = UserAccount.objects.get(user = request.user)
     context = {
-        #'user_data' : data_user,
-        #'events_joined' : data_user.events_joined.all(),
+        'user_data' : data_user,
+        'events_joined' : data_user.events_joined.all(),
     }
     return render(request, 'my_account.html', context)
 
