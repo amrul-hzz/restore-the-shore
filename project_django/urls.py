f0e0f93b44d335d4cd47259eae06bc6b3743b4cf
amanda.nurul11 <amanda.nurul11@ui.ac.id>
2022-11-01 12:59:02 +0700

    Implement view comments

--- project_django/urls.py
+++ project_django/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('example_app.urls')),
+    path('', include('landing_page.urls')),
     path('myaccount/', include('my_account.urls')),
     path('create-event/', include('create_event.urls')),
     path('leaderboard/', include('leaderboard.urls')),
