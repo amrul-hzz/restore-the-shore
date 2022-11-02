9611addafb9a17dfc3313c83aea8f85408846290
maysha.haliza <maysha.haliza@ui.ac.id>
2022-11-02 19:16:01 +0700

    add username

--- landing_page/models.py
+++ landing_page/models.py
@@ -8,9 +8,11 @@ from django.contrib.auth.models import User
 class UserAccount(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     user_point = models.IntegerField(default = 0)
+    username = models.CharField(max_length = 63)
     # import models dari Event
     #events_joined = models.ManyToManyField(Event)
 
     # post di forum yang author nya user ini bisa langsung cek dari database forum aja
 
-    
+class Feedback(models.Model):
+    message = models.TextField()
