906216ef62b8bc8b4cf65c3a87e46bb7560c1778
Amanda Nurul Izzah <108340923+amrul-hzz@users.noreply.github.com>
2022-11-02 21:03:46 +0700

    Delete landing_page directory

--- landing_page/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import UserAccount, Feedback
-# Register your models here.
-
-admin.site.register(UserAccount)
-admin.site.register(Feedback)
--- landing_page/forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm
-from landing_page.models import Feedback
-
-class FeedbackForm(ModelForm):
-    class Meta:
-        model = Feedback
-        fields = ['message']
