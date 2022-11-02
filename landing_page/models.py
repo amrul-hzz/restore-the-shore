0c663dca9e20fbd74296b446422a0f741f2e1e0a
amanda.nurul11 <amanda.nurul11@ui.ac.id>
2022-11-02 22:49:19 +0700

    Resolve conflict :)

--- landing_page/models.py
+++ landing_page/models.py
@@ -2,6 +2,7 @@ from email.policy import default
 from pyexpat import model
 from django.db import models
 from django.contrib.auth.models import User
+from create_event.models import Event
 from timeline.models import JoinEvent
 
 # Create your models here.
@@ -10,7 +11,7 @@ class UserAccount(models.Model):
     user_point = models.IntegerField(default = 0)
     username = models.CharField(max_length = 63, default = "unknown")
     # import models dari Event
-    #events_joined = models.ManyToManyField(Event)
+    events_joined = models.ManyToManyField(JoinEvent)
 
     # post di forum yang author nya user ini bisa langsung cek dari database forum aja
 
